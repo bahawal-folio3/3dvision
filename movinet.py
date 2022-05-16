3cf4752114d9f76f18c063022c5af5cd471eb2f5
bahawalkhan <bahawalkhan@folio3.com>
2022-05-16 12:45:32 +0500

    movinet model

---
+++ movinet.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+import tensorflow_hub as hub
+
+def get_model():
+    hub_url = "https://tfhub.dev/tensorflow/movinet/a2/base/kinetics-600/classification/3"
+
+    encoder = hub.KerasLayer(hub_url, trainable=True)
+
+    inputs = tf.keras.layers.Input(
+        shape=[8, 112, 112, 3],
+        dtype=tf.float32,
+        name='image')
+
+    # [batch_size, 600]
+    outputs = encoder(dict(image=inputs))
+
+    model = Sequential([
+        tf.keras.Model(inputs, outputs, name='movinet'),
+        Dense(4096, activation='relu', name='fc6'),
+        Dropout(.5),
+        Dense(4096, activation='relu', name='fc7'),
+        Dropout(.5),
+        Dense(1, activation='sigmoid', name='fc8'),
+    ])
+    return model
+
+if __name__ == "__main__":
+    model = get_model()
+    example_input = tf.ones([32, 8, 112, 112, 3])
+    example_output = model(example_input)
+    print(example_output)
