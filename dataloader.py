41ca768d9a56494878033f305296966f0441db49
bahawalkhan <bahawalkhan@folio3.com>
2022-05-18 17:48:17 +0500

    fix visualization

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
 import cv2
-from numpy import asarray
 
 from create_csv import get_dataframe
 class DataGenerator(tf.keras.utils.Sequence):
@@ -34,39 +33,34 @@ class DataGenerator(tf.keras.utils.Sequence):
         images = []
         for i in self.df.iloc[slice].img:
             image = cv2.imread(i)
-            image = cv2.resize(image,(112,112))/255.0
 
-            # cv2.imshow('img',image) #turn on to see frame being loaded
-            # cv2.waitKey(0) 
+            image = cv2.resize(image,(112,112))/255.0
             images.append(image)
         # for image in images:
         #     cv2.imshow('img',image) #turn on to see frame being loaded
         #     cv2.waitKey(0) 
-        return images,self.df.iloc[slice].label 
-
+        return images,self.df.iloc[slice].label
 
     def __get_data(self, batch):
         X = []
         y = []
         for i, id in enumerate(batch):
-            img,label = self.load_images(batch[i])
+            img, label = self.load_images(batch[i])
        
             X.append(img)
             y.append(label)
-
- 
         return np.array(X),np.array(y)
 
 if __name__=="__main__":
-    classes = ['service','everythingelse']
-    df = get_dataframe(classes,'data',window=8)
-    dataloader = DataGenerator(df)
-    for i,j in dataloader:
-
-        for j in i[0]:
-            for k in j:
-                cv2.imshow('check',k)           
-                cv2.waitKey(0)
+    classes = ['service', 'everythingelse']
+    df = get_dataframe(classes, 'data', window=8)
+    dataloader = DataGenerator(df, dim=172)
+    for i, k in dataloader:
+        for index, j in enumerate(i[0]):
+            print(j.shape)
+            print(k[index])
+            cv2.imshow('check', j)
+            cv2.waitKey(0)
 
         break
     
