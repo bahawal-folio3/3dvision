abc9b5e2a3f82cdd2f5bf0f26799f176ed78d925
bahawalkhan <bahawalkhan@folio3.com>
2022-05-18 17:49:39 +0500

    added parameters, f1 score as metrics

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
-from movinet import get_model
+from movinet import get_model, TRAINABLE
 from dataloader import DataGenerator
 from create_csv import get_dataframe
+from f1_metrics import f1_m
 # from sklearn.model_selection import train_test_split
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
@@ -8,40 +9,55 @@ import pandas as pd
 import tensorflow as tf
 import datetime
 
-classes = ['everythingelse','service']
-df = get_dataframe(classes,'data',window=5)
+
+batch_size = 24
+dim = 112
+window = 8
+model_name = 'a4'
+classes = ['service', 'everythingelse']
+df = get_dataframe(classes, 'data', window=window)
 df = df.sample(frac=1).reset_index(drop=True)
 train_size = int(df.shape[0]*0.8)
+
 train_data = df[:train_size]
 test_data = df[train_size:].reset_index(drop=True)
-val_data = df[train_size:train_size+150].reset_index(drop=True)
+val_data = df[train_size:].reset_index(drop=True)
 
 
-train_loader = DataGenerator(train_data,dim=172)
-test_loader = DataGenerator(test_data,dim=172)
-val_loader = DataGenerator(val_data,dim=172)
+train_loader = DataGenerator(train_data, batch_size=batch_size, dim=dim)
+test_loader = DataGenerator(test_data, batch_size=batch_size, dim=dim)
+val_loader = DataGenerator(val_data, batch_size=batch_size, dim=dim)
 
 
-model = get_model()
+model = get_model(model_name='model_name')
 
 
 model.compile(
   optimizer=tf.keras.optimizers.Adam(),
-  loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
-  metrics=['acc', tf.keras.metrics.AUC()])
+  loss=tf.keras.losses.CategoricalCrossentropy(),
+  metrics=['acc', tf.keras.metrics.AUC(),f1_m])
 
-log_dir = "/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=log_dir,
-    histogram_freq=1) # Enable histogram computation for every epoch.
 
-EPOCHS = 100
-checkpoint_filepath = 'checkpoint.h5'
+    log_dir=log_dir,
+    histogram_freq=0,
+    write_graph=True,
+    write_images=False,
+    write_steps_per_second=False,
+    update_freq='epoch',
+    profile_batch=0,
+    embeddings_freq=0,
+    embeddings_metadata=None,
+) # Enable histogram computation for every epoch.
+
+EPOCHS = 20
+checkpoint_filepath = f'{model_name}-f1-{window}-trainable{TRAINABLE}.h5'
 
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_filepath,
     save_weights_only=False,
-    monitor='val_acc',
+    monitor='f1_m',
     mode='max',
     save_best_only=True)
 
@@ -51,7 +67,7 @@ history = model.fit(train_loader,
                     verbose=1,
                     shuffle=True,
                     validation_data=val_loader,
-                    callbacks=[model_checkpoint_callback,tensorboard_callback]
+                    callbacks=[model_checkpoint_callback, tensorboard_callback]
                 )
 model.evaluate(test_loader)
 
